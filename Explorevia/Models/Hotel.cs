c1feb799b2999a574b986782930a7bd7816d98b4
Reham Mohamed <rwrwm956@gmail.com>
2025-10-25 15:54:50 +0300

    Initial commit

---
+++ Explorevia/Models/Hotel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Explorevia.Models
+{
+    public class Hotel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+
+        public int Rating { get; set; }
+        [Required]
+        public decimal PricePerNight { get; set; }
+
+        public virtual ICollection<BookingRequests> BookingRequests { get; set; }
+    }
+}
