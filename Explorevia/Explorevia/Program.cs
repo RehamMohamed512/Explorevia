920d8083e20d2925fa3a8c4b2fd3bca67c79acd3
manar <mostafamanar010@gmail.com>
2025-11-16 19:14:22 +0200

    frontent base

--- Explorevia/Explorevia/Program.cs
+++ Explorevia/Explorevia/Program.cs
@@ -6,8 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(option =>
-option.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+//builder.Services.AddDbContext<AppDbContext>(option =>
+//option.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
